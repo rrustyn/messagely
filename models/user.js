a63d0f846e68e7376c40917c591140548c7c60bc
Ryan Rustyn <ryanrustyn@gmail.com>
2022-08-22 11:40:49 -0700

    add register and auth user methods

--- models/user.js
+++ models/user.js
@@ -1,5 +1,10 @@
 "use strict";
 
+const bcrypt = require("bcrypt");
+const { NotFoundError } = require("../expressError");
+const db = require("../db");
+const BCRYPT_WORK_FACTOR = 12;
+
 /** User of the site. */
 
 class User {
@@ -9,11 +14,38 @@ class User {
    */
 
   static async register({ username, password, first_name, last_name, phone }) {
+
+    const hashedPassword = await bcrypt.hash(password, BCRYPT_WORK_FACTOR);
+    const result = await db.query(
+      `INSERT INTO users (username,
+                             password,
+                             first_name,
+                             last_name,
+                             phone)
+         VALUES
+           ($1, $2, $3, $4, $5)
+         RETURNING username, password, first_name, last_name, phone`,
+      [username, hashedPassword, first_name, last_name, phone]);
+
+    return result.rows[0];
+
   }
 
   /** Authenticate: is username/password valid? Returns boolean. */
 
   static async authenticate(username, password) {
+
+    const result = await db.query(
+      `SELECT password
+      FROM users
+      WHERE username = $1`,
+      [username]);
+    const user = result.rows[0];
+
+    if (user) {
+      return (await bcrypt.compare(password, user.password) === true);
+    }
+    return false;
   }
 
   /** Update last_login_at for user */
