a908492bfaf3957cc93a077db478e80843ca57f1
Ryan Rustyn <ryanrustyn@gmail.com>
2022-08-22 17:42:48 -0700

    change register route to send object to User.register

--- routes/auth.js
+++ routes/auth.js
@@ -3,20 +3,16 @@
 const Router = require("express").Router;
 const router = new Router();
 const db = require("../db");
-const {
-  authenticateJWT,
-  ensureLoggedIn,
-  ensureCorrectUser,
-} = require("../middleware/auth");
 const { UnauthorizedError } = require("../expressError");
 const User = require("../models/user");
 const { SECRET_KEY } = require("../config");
+const jwt = require("jsonwebtoken");
 
 /** POST /login: {username, password} => {token} */
 router.post("/login", async function (req, res) {
   const { username, password } = req.body;
-
-  if (User.authenticate(username, password)) {
+  
+  if (await User.authenticate(username, password)) {
     const token = jwt.sign({ username }, SECRET_KEY);
     return res.json({ token });
   }
@@ -29,11 +25,9 @@ router.post("/login", async function (req, res) {
  * {username, password, first_name, last_name, phone} => {token}.
  */
 router.post("/register", async function (req, res) {
-  const { username, password, first_name, last_name, phone } = req.body;
+  const user = await User.register(req.body);
   
-  User.register(username, password, first_name, last_name, phone);
-  
-  const token = jwt.sign({ username }, SECRET_KEY);
+  const token = jwt.sign({ username: user.username }, SECRET_KEY);
   return res.json({ token });
 });
 
